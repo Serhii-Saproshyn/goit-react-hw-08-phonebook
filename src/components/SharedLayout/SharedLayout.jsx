f9d0f433bf994c8411f0187b850cc584ff8181c9
Serhii Saproshyn <112710917+Serhii-Saproshyn@users.noreply.github.com>
2023-05-29 01:00:59 +0300

    handle erroros and loader

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -3,12 +3,12 @@ import UserMenu from 'components/UserMenu/UserMenu';
 import { Suspense } from 'react';
 import { useSelector } from 'react-redux';
 import { Outlet } from 'react-router-dom';
-import { Watch } from 'react-loader-spinner';
+// import { Watch } from 'react-loader-spinner';
 import css from './SharedLayout.module.css';
 
 const SharedLayout = () => {
   const token = useSelector(state => state.auth.token);
-  const isLoading = useSelector(state => state.auth.isLoading);
+  // const isLoading = useSelector(state => state.auth.isLoading);
   return (
     <>
       <header className={css.headerLayout}>
